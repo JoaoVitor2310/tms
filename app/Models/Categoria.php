d5b085b48fe931666b0524fd4bb6079ace76df29
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2025-05-06 20:38:28 -0300

    chore: relation ser e categoria

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -12,4 +12,9 @@ class Categoria extends Model
     {
         return $this->hasMany(Anotacao::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function categorias()
+    {
+        return $this->hasMany(Categoria::class);
+    }
 }
