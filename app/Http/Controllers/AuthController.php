80187a72cf8a49a0f090177b9af2d2a88b609952
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2025-05-07 20:17:46 -0300

    feat: rota de logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -58,4 +58,14 @@ class AuthController extends Controller
 
         return redirect()->route('dashboard');
     }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect()->route('loginForm');
+    }
 }
