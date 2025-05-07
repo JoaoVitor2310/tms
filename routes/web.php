450ffb0d4cd368e1203b9c62a02d34dc37868624
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2025-05-07 20:28:15 -0300

    feat: middleware auth, logout e rotas resource de categorias, e anotações

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,32 @@
 <?php
 
+use App\Http\Controllers\AnotacaoController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {return view('dashboard');})->name('dashboard');
-
-// Autenticação
 Route::get('/login', [AuthController::class, 'loginForm'])->name('loginForm');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 
 Route::get('/register', [AuthController::class, 'registerForm'])->name('registerForm');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Autenticação
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    //Anotacoes
+    Route::resource('/anotacoes', AnotacaoController::class);
+
+    //Categorias
+    Route::resource('/categorias', CategoriaController::class);
+});
 
 // Fallback route para redirecionar qualquer URL inválida
 Route::fallback(function () {
