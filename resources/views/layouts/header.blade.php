d44fa273b4f64a27373033896ded9adf821df3d0
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2025-05-08 21:38:22 -0300

    feat: header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,14 +1,8 @@
-<header class="navbar navbar-expand-lg d-none d-md-block" style="background-color: #1F3C76;">
-    <h1>Header</h1>
-    <div class="container-fluid">
-        {{-- <a class="navbar-brand text-white fw-bold" href="{{ route('home') }}">
-            Meu Sistema
-        </a> --}}
-
-        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-light text-decoration-none">
-            <span><img src="{{asset('images/logo-tms-black.png')}}" alt="Logo"
-                    style="width: 50px; height: 50px;"></span>
-            <span class="fs-4 ml-2 text-nowrap">TMS</span>
+<header class="navbar navbar-expand-lg" style="background-color: #999898;">
+    <div class="container-fluid" style="max-width: 90%;">
+        <a href="/" class="d-flex align-items-center text-light text-decoration-none me-3">
+            <img src="{{ asset('images/logo-tms-black.png') }}" alt="Logo" style="width: 50px;" class="img-fluid me-2">
+            <span class="fs-4 text-nowrap">TMS</span>
         </a>
 
         <button class="navbar-toggler text-white" type="button" data-bs-toggle="collapse"
@@ -16,37 +10,31 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse justify-content-end" id="navbarContent">
+        <div class="collapse navbar-collapse justify-content-center justify-content-md-between" id="navbarContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="{{ route('anotacoes.index') }}">Anotações</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="{{ route('categorias.index') }}">Categorias</a>
+                </li>
+            </ul>
+
             <div class="dropdown">
                 <a href="#" class="d-flex align-items-center link-light text-decoration-none dropdown-toggle"
                     id="dropdownUser2" data-bs-toggle="dropdown" aria-expanded="false">
                     <strong>{{ Auth::user()->name }}</strong>
                 </a>
-                <ul class="dropdown-menu dropdown-menu-end text-small shadow" aria-labelledby="dropdownUser2">
-                    @guest
-                        <li><a class="dropdown-item" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                        @if (Route::has('register'))
-                            <li><a class="dropdown-item" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @endif
-                    @else
-                        <li>
-                            <a class="dropdown-item" href="{{ route('users.edit', Auth::user()->id) }}">
-                                <i class="bi bi-person-circle text-primary"></i> Meu perfil
-                            </a>
-                        </li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li>
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                <i class="bi bi-box-arrow-left text-primary"></i> {{ __('Logout') }}
-                            </a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </li>
-                    @endguest
+                <ul class="dropdown-menu dropdown-menu-start dropdown-menu-lg-end text-small shadow" aria-labelledby="dropdownUser2">
+                    <li>
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="bi bi-box-arrow-left text-danger"></i> {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
+                    </li>
                 </ul>
             </div>
         </div>
